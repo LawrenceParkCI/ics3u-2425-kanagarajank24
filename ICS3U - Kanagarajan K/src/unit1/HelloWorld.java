5e436baea6740396919f90943a8a4cf9e25e2581
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2024-09-20 11:11:16 -0400

    Adding ICS3U project that includes HelloWorld class.

---
+++ ICS3U - Kanagarajan K/src/unit1/HelloWorld.java
@@ -0,0 +1,24 @@
+package unit1;
+/**
+ * Description: This programs prints Hello World, and a second statement. <br> 
+ * Date: September 19, 2024 
+ * @author Kaleena Kanagarajan
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This print hello to the system console
+		System.out.println("Hello World!");
+		System.out.println("Bonjour le monde!");
+		/*
+		 * This is a block comment 
+		 * <br> is break line in HTML]
+		 * 
+		 * Three types of comments
+		 * the slash double star comment creates a Javadoc block comment
+		 * the slash single star creates a normal block comment 
+		 *  double slash is a normal single line comment
+		 */
+	}
+
+}
