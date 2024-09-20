babce7eeab58ae9d83697e1b9a619ea3e62edb6a
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2024-09-20 11:41:31 -0400

    This is my AboutMe class.

---
+++ ICS3U - Kanagarajan K/src/unit1/AboutMe.java
@@ -0,0 +1,24 @@
+package unit1;
+/**
+ * Description: This programs describes things about me (name, grade, teacher and school) <br>
+ * Date: September 20, 2024
+ * @author Kaleena Kanagarajan
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// This statement is the title
+		System.out.println("About Me :)\n-----------");
+
+		// These statements describe my name, grade, school and teacher
+		System.out.println("\nMy name is Kaleena Kanagarajan.");
+
+		System.out.println("I am in the 11th grade at Lawrence Park Collegiate Insitute.");
+
+		System.out.println("My teacher is Ms. Kemp.");
+
+
+
+	}
+
+}
