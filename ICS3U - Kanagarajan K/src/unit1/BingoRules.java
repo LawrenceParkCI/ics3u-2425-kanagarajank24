6ab51ee681350d77199de5e3e8dfe3d8c6ff256e
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2024-09-24 11:39:14 -0400

    This includes my BingoRules class.

---
+++ ICS3U - Kanagarajan K/src/unit1/BingoRules.java
@@ -0,0 +1,26 @@
+/**
+ * Description: This programs describes the rules of Bingo and gives an example of a Bingo card. <br>
+ * Date: September 24, 2024
+ * @author Kaleena Kanagarajan
+ */
+package unit1;
+
+public class BingoRules {
+
+	public static void main(String[] args) {
+		//This statement prints the title
+		System.out.println("How to Play Bingo");
+		System.out.println("-----------------");
+		
+		//These statements print the rules of Bingo
+		System.out.println("1. The caller randomly pulls a numbered bingo ball.");
+		System.out.println("2. The number is placed on the bingo board and called out.");
+		System.out.println("3. Players look for the called number on their bingo card.");
+		System.out.println("4. If the number is located, it is marked off.");
+		System.out.println("5. Steps 1 to 4 are repeated until a play matches the BINGO pattern.");
+		System.out.println("6. The winning player yells BINGO!");
+		
+
+	}
+
+}
