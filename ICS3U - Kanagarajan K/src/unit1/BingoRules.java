313972c29890a218a76859844fdeb1fad5a9c71b
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2024-09-25 10:39:24 -0400

    This is my Bingo Rules class, and it now has my example Bingo Card.

--- ICS3U - Kanagarajan K/src/unit1/BingoRules.java
+++ ICS3U - Kanagarajan K/src/unit1/BingoRules.java
@@ -20,7 +20,18 @@ public class BingoRules {
 		System.out.println("5. Steps 1 to 4 are repeated until a play matches the BINGO pattern.");
 		System.out.println("6. The winning player yells BINGO!");
 		
-
+		//These statements print out an example of a Bingo card
+		System.out.println("\nBingo Card");
+		System.out.println("----------");
+		System.out.println("_______________________");
+		System.out.printf("|%-5s%-5s%-5s%-5s%-1s|", "B", "I", "N", "G", "O");
+		System.out.println("\n|---------------------|");
+		System.out.printf("|%-4s%-5s%-5s%-5s%-1s|", "2", "16", "32", "48", "62");
+		System.out.printf("\n|%-4s%-5s%-5s%-5s%-1s|", "5", "19", "35", "52", "74");
+		System.out.printf("\n|%-4s%-4s%-6s%-5s%-1s|", "4", "22", "Free", "59", "65");
+		System.out.printf("\n|%-4s%-5s%-5s%-5s%-1s|", "7", "27", "43", "56", "72");
+		System.out.printf("\n|%-4s%-5s%-5s%-5s%-1s|", "6", "20", "39", "58", "63");
+		System.out.println("\n|_____________________|");
 	}
 
 }
