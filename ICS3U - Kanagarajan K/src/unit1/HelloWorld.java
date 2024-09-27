2ad0246c7ec330f1a62dc9c52f8b1e145f8acb3e
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2024-09-27 09:50:41 -0400

    This includes my Hello World class, and I have added a comment above my method.

--- ICS3U - Kanagarajan K/src/unit1/HelloWorld.java
+++ ICS3U - Kanagarajan K/src/unit1/HelloWorld.java
@@ -1,19 +1,24 @@
 package unit1;
 /**
- * Description: This programs prints Hello World, and a second statement. <br> 
+ * Description: This programs prints Hello World, and two other statements. <br> 
  * Date: September 19, 2024 
  * @author Kaleena Kanagarajan
  */
 public class HelloWorld {
-
+	
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
 		//This print hello to the system console
 		System.out.println("Hello World!");
 		System.out.println("Bonjour Le Monde!");
 		System.out.println("Hola The World");
+		
 		/*
 		 * This is a block comment 
-		 * <br> is break line in HTML]
+		 * <br> is break line in HTML
 		 * 
 		 * Three types of comments
 		 * the slash double star comment creates a Javadoc block comment
