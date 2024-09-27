cfca72d2290e689785a02c9ed19b3dd446eff8ab
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2024-09-27 09:52:35 -0400

    This includes my About Me class, and I have added a comment above my method.

--- ICS3U - Kanagarajan K/src/unit1/AboutMe.java
+++ ICS3U - Kanagarajan K/src/unit1/AboutMe.java
@@ -1,23 +1,27 @@
 package unit1;
 /**
  * Description: This programs describes things about me (name, grade, teacher and school) <br>
- * Date: September 20, 2024
+ * Date: September 21, 2024
  * @author Kaleena Kanagarajan
  */
 public class AboutMe {
-
+	
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
 		// This statement is the title
 		System.out.println("About Me \n--------");
 
-		// These statements describe my name, grade, school and teacher
+		// These statements describe my name, grade, school, teacher, and a cheer for my school
 		System.out.println("\nMy name is Kaleena Kanagarajan.");
 
 		System.out.println("I am in the 11th grade at Lawrence Park Collegiate Insitute.");
 
 		System.out.println("My teacher is Ms. Kemp.");
 		
-		System.out.println("\"Go Panthers\"");
+		System.out.println("\"Go Panthers!!\"");
 		
 		//These statements print out my schedule this week 
 		System.out.println("\nMy School Schedule");
