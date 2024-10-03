972c32e2e24b9cdd8c61884d98e67db5ddb86886
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2024-10-03 10:14:50 -0400

    This includes my new PlayingWithStrings class.

---
+++ ICS3U - Kanagarajan K/src/unit1/PlayingWithStrings.java
@@ -0,0 +1,17 @@
+package unit1;
+/**
+ * Description: This programs follows the Working with Strings Worksheet. <br>
+ * Date: Oct 4, 2024
+ * @author Kaleena Kanagarajan
+ */
+public class PlayingWithStrings {
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
