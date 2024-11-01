4533e07e37731c07f22ae704301b2393bdbcb19b
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2024-11-01 10:17:12 -0400

    This includes my new OddEven class and PostitveNegitive class.

---
+++ ICS3U - Kanagarajan K/src/unit2/PositiveNegitive.java
@@ -0,0 +1,26 @@
+package unit2;
+
+import java.util.Scanner;
+/**
+ * Description: This program takes an integer from the user and returns if is  <br>
+ * Date: November 1, 2024
+ * @author Kaleena Kanagarajan
+ */
+
+public class PositiveNegitive {
+
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Variables
+		int posNeg;
+		
+		//check if poss
+
+	}
+
+}
