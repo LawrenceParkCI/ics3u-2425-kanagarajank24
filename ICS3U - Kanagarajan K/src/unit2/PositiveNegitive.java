2f89fe957029754fc6ecfd20d9c8bfb7bde5bb74
Kaleena-K <kaleena.kanagarajan@student.tdsb.on.ca>
2024-11-01 10:59:43 -0400

    This includes my PositiveNegative Class

--- ICS3U - Kanagarajan K/src/unit2/PositiveNegitive.java
+++ ICS3U - Kanagarajan K/src/unit2/PositiveNegitive.java
@@ -2,7 +2,7 @@ package unit2;
 
 import java.util.Scanner;
 /**
- * Description: This program takes an integer from the user and returns if is  <br>
+ * Description: This program takes an integer from the user and returns if is positive or negitive and if it is divisible by 7.<br>
  * Date: November 1, 2024
  * @author Kaleena Kanagarajan
  */
@@ -18,8 +18,34 @@ public class PositiveNegitive {
 		
 		//Variables
 		int posNeg;
+
+		//Get user input
+		System.out.println("Please input an integer and <Enter>");
+		posNeg = sc.nextInt();
 		
-		//check if poss
+		//Check if positive or negitive and tell user
+		if (posNeg == 0) {
+			System.out.println("Your number is neither negitive nor positive. It is 0!");
+		}
+		else{
+			if (posNeg > 0) {
+				System.out.println("Your number is positive!");
+			}
+			else{
+				System.out.println("Your number is negitive!");
+			}
+			
+		}
+
+		//Check if it is divisible by 7 and tell user
+		if (posNeg % 7 == 0){
+			System.out.println("Your number is divisible by 7 :)");
+		}
+		else{
+			System.out.println("Your number is not divisble by 7 :(");
+		}
+
+		sc.close();
 
 	}
 
