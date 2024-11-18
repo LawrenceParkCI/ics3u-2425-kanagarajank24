60be3102d63637e387ac65d7e2d9ec8708420430
Dell5480 <Dell5480@192.168.2.67>
2024-11-18 17:47:01 -0500

    This includes my completed Double Dice Contest and Name Game classes.

--- ICS3U - Kanagarajan K/src/unit2/NameGame.java
+++ ICS3U - Kanagarajan K/src/unit2/NameGame.java
@@ -1,4 +1,5 @@
 package unit2;
+import java.util.Scanner;
 /**
  * Description: This follows the Name Game worksheet.
  * Date: Nov 18, 2024
@@ -17,7 +18,24 @@ public class NameGame {
 		 * and 30% of the time says they hate that name.
 		 * */
 
+		Scanner sc = new Scanner (System.in);
 
+		//Variables
+		String name;
+		int random = (int)(Math.random() * 10);
+
+		//Get user's name
+		System.out.println("What is your name?");
+		name = sc.nextLine();
+
+		if(random < 6) {
+			System.out.println( name + "! That's my favourite name ( ˶ˆᗜˆ˵ )");
+		}
+		else {
+			System.out.println(name + "! I hate that name ᕦ(ò_óˇ)ᕤ");
+		}
+
+		sc.close();
 	}
 
 }
