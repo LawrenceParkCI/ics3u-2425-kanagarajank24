784d9198feb3371d4ad4490e099830cac143adc2
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2025-01-09 10:13:13 -0500

    This contains my WordleProtoype class, where I have begun to work on my final project.

--- ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
+++ ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
@@ -18,53 +18,92 @@ public class WordlePrototype {
 		Scanner sc = new Scanner(System.in);
 		String [] words = {"weary", "happy", "magic", "shift", "ruins"};
 
-		//Method is needed. this wont work. Or make word a global variable
+		//Chose a random word from words array 
 		int randomNum = (int) (Math.random()*(words.length));
 		String word = words[randomNum];
-		String user;
-		char [] correct;
-
-		user = sc.nextLine();
-		char[] userArray = user.toCharArray();
+		System.out.println("The word is: " + word);
 		
-//		for(int i = 0; i < guess.length; i ++) {
-//			System.out.println(guess[i]);
-//		}
+		int [] correct = new int [5];
+				
+		//Get user input
+		System.out.println("What is your guess?");
+		String user = sc.nextLine();
+		char[] userArray = user.toLowerCase().toCharArray();
 		
-		System.out.println(containsLetter(userArray, word));
-		char [] c = containsLetter(userArray, word);
+		//test contansLetter
+		
+		int [] test = containsLetter(userArray, word);
+		
 		for(int i = 0; i < 5; i++) {
-			System.out.println(containsLetter(userArray, word));
+			System.out.println(test[i]);
 		}
-//		
+		
+
 		sc.close();
 
 	}
-
-	//when checking if green or yellow, check arrays duplicate letters or make array with 5 charcters, but ir correct, put 0 in chars place
+	
+	//how do I check for duplicates?
+	//when checking if green or yellow, check arrays duplicate letters or make array with 5 characters, but if correct, put 0 in chars place
+	
+	
+	//Maybe combine the methods
+	//This Method Words
 	/**
 	 *Description: This method checks if any of the letters in the user’s guess are in the correct word.
 	 *@param userArray -> This is the user’s guess, which is stored in an array of characters
 	 *@param word -> This is the correct word
 	 *@return an array of integers where 0s will represent incorrect letters, and correct will be 1s.
 	 */
-	
-	public static char []containsLetter(char[] userArray, String word) {
-		char[] containsLetter = new char [5];
+	public static int []containsLetter(char[] userArray, String word) {
+		int [] containsLetter = new int [5];
+		int indexOf;
+		
+		//to check for duplicates
+		boolean [] dupes = {true, true, true, true, true};
 		
 		for (int i = 0; i < 5; i++) {
 			
-			if(word.indexOf(userArray[i]) == -1) {
-				containsLetter[i] = 0;
+			indexOf = word.indexOf(userArray[i]);
+
+			
+			if(dupes[indexOf] && indexOf != -1 ) {
+				containsLetter[i] = 1;
+				dupes[indexOf] = false;
+
 			}
 			else {
-				containsLetter[i] = 1;
+				containsLetter[i] = 0;
+				
 			}
-			
+		
 		}
 		
 		return containsLetter;
 	}
+	
+	//This Methods Works
+	/**
+	 *Description: This method checks if the correct letters in user’s guess are in the correct place
+	 *@param  userArray -> This is the user’s guess, which is stored in an array of characters
+	 *@param word -> This is the correct word
+	 *@return an array of integers where 0s will represent letters with incorrect placement, and 2s for correct letter and placement
+	 */
+	public static int []correctPlace(char[] userArray, String word) {
+		int [] correctPlace = containsLetter(userArray, word);
+		
+		for (int i = 0; i < 5; i++) {
+			
+			if(word.charAt(i) == userArray[i]) {
+				correctPlace[i] = 2;
+			}
+		}
+		
+		return correctPlace;
+	}
+	
+	
+
 
 
 }
