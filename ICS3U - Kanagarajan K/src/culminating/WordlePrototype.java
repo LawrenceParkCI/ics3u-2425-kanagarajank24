8bb1649668b12c9497bef193e02cedae2a6855c4
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2025-01-10 11:40:17 -0500

    This includes my WordlePrototype class, where I have added the HSA console.

--- ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
+++ ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
@@ -1,8 +1,12 @@
 package culminating;
+import java.awt.Color;
+import java.awt.Font;
 import java.util.Scanner;
 
+import hsa_new.Console;
+
 /**
-Description:This program is where I am planning and testing my Wordle Final project. <br>
+Description: This program is where I am planning and testing my Wordle Final project. <br>
 Date: Jan 8, 2025
 @author Kaleena Kanagarajan
  */
@@ -12,41 +16,98 @@ public class WordlePrototype {
 	 * This is the entry point to the program
 	 * @param args unused
 	 */	
-	//static Console c; <- add this in real program so that the console applys to all methods?
+	//static Console c; <- add this in real program so that the console applies to all methods?
 	public static void main(String[] args) {
-
+		
+		/*
+		 * Things to add
+		 * - Handle User error
+		 * - Comment
+		 * - Graphics
+		 * - Music
+		 * - If I have time, add score storing method
+		 */
+		Console c = new Console(100, 150, 10, "Wordle");
+		
 		Scanner sc = new Scanner(System.in);
 		String [] words = {"weary", "happy", "magic", "shift", "ruins"};
+		String [] output = new String [5];
+		int guess = 0;
+
+
+		//User input
+		String user;
+		char[] userArray = new char [5];
+
 
 		//Chose a random word from words array 
 		int randomNum = (int) (Math.random()*(words.length));
 		String word = words[randomNum];
 		System.out.println("The word is: " + word);
-		
+
 		int [] correct = new int [5];
-				
-		//Get user input
-		System.out.println("What is your guess?");
-		String user = sc.nextLine();
-		char[] userArray = user.toLowerCase().toCharArray();
 		
-		//test contansLetter
 		
-		int [] test = containsLetter(userArray, word);
 		
-		for(int i = 0; i < 5; i++) {
-			System.out.println(test[i]);
+		
+		
+		//Intro
+		c.setColor(new Color(120, 178, 123));
+		c.fillRect(0, 0, 1000, 1000);
+		c.setTextColor(Color.WHITE);
+		c.setColor(Color.WHITE);
+		c.setFont(new Font("SansSerif", Font.PLAIN, 100));
+		c.drawString("Wordle", 300, 500);
+		//c.setTextBackgroundColor(Color.GREEN);
+		//c.print("Hi");
+
+
+		//test containsLetter
+
+		//int [] test = ;
+
+		//guess
+		for(int i = 0; i < 6; i++) {
+
+			//Get user input
+			System.out.println("What is your guess?");
+			user = sc.nextLine();
+			userArray = user.toLowerCase().toCharArray();
+			
+			correct = correctPlace(userArray, word, containsLetter(userArray, word));
+			
+			output = output(userArray, correctPlace(userArray, word, containsLetter(userArray, word)));
+
+			//Print out guess to grid
+			System.out.printf("%-5s%-5s%-5s%-5s%-5s\n\n", output[0], output[1], output[2], output[3], output[4]);
+			
+			if(word.equals(user)) {
+				break;
+			}
+			
+			guess ++;
+		
 		}
 		
+		if(guess < 6) {
+			System.out.println("You guessed the word!");
+		}
+		else {
+			System.out.println("The word was " + word + ". You lose :(");
+		}
+	
+		
+		
+
 
 		sc.close();
 
 	}
-	
+
 	//how do I check for duplicates?
 	//when checking if green or yellow, check arrays duplicate letters or make array with 5 characters, but if correct, put 0 in chars place
-	
-	
+
+
 	//Maybe combine the methods
 	//This Method Words
 	/**
@@ -56,53 +117,77 @@ public class WordlePrototype {
 	 *@return an array of integers where 0s will represent incorrect letters, and correct will be 1s.
 	 */
 	public static int []containsLetter(char[] userArray, String word) {
-		int [] containsLetter = new int [5];
+		int [] correctness = new int [5];
 		int indexOf;
-		
+
 		//to check for duplicates
 		boolean [] dupes = {true, true, true, true, true};
-		
+
 		for (int i = 0; i < 5; i++) {
-			
+
 			indexOf = word.indexOf(userArray[i]);
 
-			
-			if(dupes[indexOf] && indexOf != -1 ) {
-				containsLetter[i] = 1;
+
+			if(indexOf != -1 && dupes[indexOf]) {
+				correctness[i] = 1;
 				dupes[indexOf] = false;
 
 			}
 			else {
-				containsLetter[i] = 0;
-				
+				correctness[i] = 0;
+
 			}
-		
+
 		}
-		
-		return containsLetter;
+
+		return correctness;
 	}
-	
+
 	//This Methods Works
 	/**
 	 *Description: This method checks if the correct letters in user’s guess are in the correct place
 	 *@param  userArray -> This is the user’s guess, which is stored in an array of characters
 	 *@param word -> This is the correct word
+	 *@param correctness -> array, which is returned from containsLetter method, that stores how correct each letter in the user's guess is
 	 *@return an array of integers where 0s will represent letters with incorrect placement, and 2s for correct letter and placement
 	 */
-	public static int []correctPlace(char[] userArray, String word) {
-		int [] correctPlace = containsLetter(userArray, word);
-		
+	public static int []correctPlace(char[] userArray, String word, int [] correctness) {
+
 		for (int i = 0; i < 5; i++) {
-			
+
 			if(word.charAt(i) == userArray[i]) {
-				correctPlace[i] = 2;
+				correctness[i] = 2;
+			}
+		}
+
+		return correctness;
+	}
+
+	/**
+	 *Description: This method will create the output for the letter based on their correctness
+	 *@param  userArray -> This is the user’s guess, which is stored in an array of characters
+	 *@param correctness -> the array which holds the correctness of each letter in the user's guess, which is returned from correctPlace
+	 *@return an array of string where the a letter and symbol according to correctness (x for wrong, - for wrong placement and + for correct) will be stored.
+	 */
+	public static String []output(char[] userArray, int[] correctness) {
+		String[] output = new String [5];
+
+		for(int i = 0; i < 5; i ++) {
+			if(correctness[i] == 0) {
+				output[i] =  userArray[i] + "x";
+			}
+			else if(correctness[i] == 1) {
+				output[i] =  userArray[i] + "-";
+			}
+			else {
+				output[i] =  userArray[i] + "+";
 			}
 		}
 		
-		return correctPlace;
+		return output;
 	}
-	
-	
+
+
 
 
 
