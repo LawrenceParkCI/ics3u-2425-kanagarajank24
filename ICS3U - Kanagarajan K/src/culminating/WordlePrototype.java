0cc588fc4eb6586de6e07779d2698c39ca925af8
342943099 <342943099@C-MXL2393F1H.tdsb.on.ca>
2025-01-13 10:14:40 -0500

    This includes my WordlePrototype Class

--- ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
+++ ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
@@ -15,9 +15,10 @@ public class WordlePrototype {
 	/**
 	 * This is the entry point to the program
 	 * @param args unused
+	 * @throws InterruptedException 
 	 */	
 	//static Console c; <- add this in real program so that the console applies to all methods?
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		/*
 		 * Things to add
@@ -52,14 +53,62 @@ public class WordlePrototype {
 		
 		
 		//Intro
-		c.setColor(new Color(120, 178, 123));
+		/*
+		c.setColor(new Color(120, 178, 123)); 
 		c.fillRect(0, 0, 1000, 1000);
-		c.setTextColor(Color.WHITE);
+		c.setTextColor(Color.WHITE); 
+		c.setColor(Color.WHITE); 
+		c.setFont(new Font("SansSerif", Font.PLAIN, 100)); 
+		c.drawString("Wordle", 290, 475);
+		  
+		//Thread.sleep(1000); 
+		c.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		c.drawString("Hit any key to continue", 350, 525);
+		  
+		c.getChar(); 
+		c.clear();
+		 */
+		//Instructions 
+		
+		c.setColor(Color.BLACK);
+		c.setFont(new Font("SansSerif", Font.BOLD, 70));
+		c.drawString("How to Play", 70, 100);
+		
+		c.setFont(new Font("SansSerif", Font.PLAIN, 30));
+		c.drawString("- You have 6 chances to guess the wordle", 100, 200);
+		c.drawString("- The colour of the tiles will change to show you how", 100, 265);
+		c.drawString("  close you were", 100, 305);
+		
+		c.setFont(new Font("SansSerif", Font.BOLD, 40));
+		c.drawString("Examples", 70, 405);
+		
+		c.setColor(new Color(120, 178, 123)); 
+		c.fillRect(100, 450, 70, 70);
+		
+		c.setColor(new Color(0, 0, 0)); 
+		c.drawRect(100, 450, 70, 70);
+		c.drawRect(190, 450, 70, 70);
+		c.drawRect(280, 450, 70, 70);
+		c.drawRect(370, 450, 70, 70);
+		c.drawRect(460, 450, 70, 70);
+		
 		c.setColor(Color.WHITE);
-		c.setFont(new Font("SansSerif", Font.PLAIN, 100));
-		c.drawString("Wordle", 300, 500);
-		//c.setTextBackgroundColor(Color.GREEN);
-		//c.print("Hi");
+		c.setFont(new Font("SansSerif", Font.BOLD, 40));
+		c.drawString("W", 115, 500);
+		
+		c.setColor(Color.BLACK);
+		c.drawString("O", 210, 500);
+		c.drawString("R", 300, 500);
+		c.drawString("D", 390, 500);
+		c.drawString("Y", 480, 500);
+		
+		c.setFont(new Font("SansSerif", Font.BOLD, 40));
+		c.drawString("W", 100, 570);
+		c.setFont(new Font("SansSerif", Font.PLAIN, 40));
+		c.drawString("     is in the word and in the correct spot", 100, 570);
+		
+		
+
 
 
 		//test containsLetter
