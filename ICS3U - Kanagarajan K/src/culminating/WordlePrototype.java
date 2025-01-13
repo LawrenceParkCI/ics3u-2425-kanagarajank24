941dca7fe0a4f42910811cd313e9d9fffef6068b
Kaleena-K <kaleena.kanagarajan@student.tdsb.on.ca>
2025-01-13 18:51:33 -0500

    This includes my WordlePrototype class

--- ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
+++ ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
@@ -31,7 +31,7 @@ public class WordlePrototype {
 		Console c = new Console(100, 150, 10, "Wordle");
 		
 		Scanner sc = new Scanner(System.in);
-		String [] words = {"weary", "happy", "magic", "shift", "ruins"};
+		String [] words = {"WEARY", "HAPPY", "MAGIC", "SHIFT", "RUINS"};
 		String [] output = new String [5];
 		int guess = 0;
 
@@ -61,13 +61,13 @@ public class WordlePrototype {
 		c.setFont(new Font("SansSerif", Font.PLAIN, 100)); 
 		c.drawString("Wordle", 290, 475);
 		  
-		//Thread.sleep(1000); 
+		Thread.sleep(1000); 
 		c.setFont(new Font("SansSerif", Font.PLAIN, 20));
 		c.drawString("Hit any key to continue", 350, 525);
 		  
 		c.getChar(); 
 		c.clear();
-		 */
+		 //
 		//Instructions 
 		
 		c.setColor(Color.BLACK);
@@ -82,10 +82,11 @@ public class WordlePrototype {
 		c.setFont(new Font("SansSerif", Font.BOLD, 40));
 		c.drawString("Examples", 70, 405);
 		
+		//Green example
 		c.setColor(new Color(120, 178, 123)); 
 		c.fillRect(100, 450, 70, 70);
 		
-		c.setColor(new Color(0, 0, 0)); 
+		c.setColor(Color.BLACK); 
 		c.drawRect(100, 450, 70, 70);
 		c.drawRect(190, 450, 70, 70);
 		c.drawRect(280, 450, 70, 70);
@@ -107,8 +108,125 @@ public class WordlePrototype {
 		c.setFont(new Font("SansSerif", Font.PLAIN, 40));
 		c.drawString("     is in the word and in the correct spot", 100, 570);
 		
+		//Yellow example
+		c.setColor(Color.BLACK); 
+		c.drawRect(100, 620, 70, 70);
 		
-
+		c.setColor(new Color(219, 204, 90)); 
+		c.fillRect(190, 620, 70, 70);
+		
+		c.setColor(Color.BLACK);  
+		c.drawRect(190, 620, 70, 70);
+		c.drawRect(280, 620, 70, 70);
+		c.drawRect(370, 620, 70, 70);
+		c.drawRect(460, 620, 70, 70);
+		
+		
+		c.setFont(new Font("SansSerif", Font.BOLD, 40));
+		c.drawString("L", 120, 670);
+		
+		c.setColor(Color.WHITE);
+		c.drawString("I", 220, 670);
+		
+		c.setColor(Color.BLACK);
+		c.drawString("G", 300, 670);
+		c.drawString("H", 390, 670);
+		c.drawString("T", 480, 670);
+		
+		c.setFont(new Font("SansSerif", Font.BOLD, 40));
+		c.drawString("I", 100, 740);
+		c.setFont(new Font("SansSerif", Font.PLAIN, 40));
+		c.drawString("   is in the word but in the wrong spot", 100, 740);
+		
+		//Grey example
+				
+		c.setColor(Color.BLACK); 
+		c.drawRect(100, 790, 70, 70);
+		
+		c.setColor(Color.GRAY); 
+		c.fillRect(370, 790, 70, 70);
+		
+		c.setColor(Color.BLACK);  
+		c.drawRect(190, 790, 70, 70);
+		c.drawRect(280, 790, 70, 70);
+		c.drawRect(370, 790, 70, 70);
+		c.drawRect(460, 790, 70, 70);
+		
+		
+		c.setFont(new Font("SansSerif", Font.BOLD, 40));
+		c.drawString("R", 120, 840);
+		c.drawString("O", 220, 840);
+		c.drawString("G", 300, 840);
+		
+		c.setColor(Color.WHITE);
+		c.drawString("U", 390, 840);
+		
+		c.setColor(Color.BLACK);
+		c.drawString("E", 480, 840);
+		
+		c.setFont(new Font("SansSerif", Font.BOLD, 40));
+		c.drawString("U", 100, 910);
+		c.setFont(new Font("SansSerif", Font.PLAIN, 40));
+		c.drawString("    is not in the word in any spot", 100, 910);
+		
+		//Continue
+		Thread.sleep(1000);
+		c.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		c.drawString("Hit any key to continue", 350, 940);
+		c.getChar();
+		
+		c.clear();
+		*/
+		
+		
+		
+		c.setColor(Color.BLACK);
+		
+		c.setFont(new Font("SansSerif", Font.PLAIN, 70));
+		c.drawString("Wordle", 330, 150);
+		
+		//Guess 1
+		c.drawRect(230, 240, 70, 70);
+		c.drawRect(320, 240, 70, 70);
+		c.drawRect(410, 240, 70, 70);
+		c.drawRect(500, 240, 70, 70);
+		c.drawRect(590, 240, 70, 70);
+		
+		//Guess 2
+		c.drawRect(230, 330, 70, 70);
+		c.drawRect(320, 330, 70, 70);
+		c.drawRect(410, 330, 70, 70);
+		c.drawRect(500, 330, 70, 70);
+		c.drawRect(590, 330, 70, 70);
+		
+		//Guess 3
+		c.drawRect(230, 420, 70, 70);
+		c.drawRect(320, 420, 70, 70);
+		c.drawRect(410, 420, 70, 70);
+		c.drawRect(500, 420, 70, 70);
+		c.drawRect(590, 420, 70, 70);
+		
+		//Guess 4
+		c.drawRect(230, 510, 70, 70);
+		c.drawRect(320, 510, 70, 70);
+		c.drawRect(410, 510, 70, 70);
+		c.drawRect(500, 510, 70, 70);
+		c.drawRect(590, 510, 70, 70);
+		
+		//Guess 5
+		c.drawRect(230, 600, 70, 70);
+		c.drawRect(320, 600, 70, 70);
+		c.drawRect(410, 600, 70, 70);
+		c.drawRect(500, 600, 70, 70);
+		c.drawRect(590, 600, 70, 70);
+		
+		//Guess 6
+		c.drawRect(230, 690, 70, 70);
+		c.drawRect(320, 690, 70, 70);
+		c.drawRect(410, 690, 70, 70);
+		c.drawRect(500, 690, 70, 70);
+		c.drawRect(590, 690, 70, 70);
+		
 
 
 		//test containsLetter
@@ -116,21 +234,60 @@ public class WordlePrototype {
 		//int [] test = ;
 
 		//guess
+		
+		
+		
 		for(int i = 0; i < 6; i++) {
 
 			//Get user input
-			System.out.println("What is your guess?");
-			user = sc.nextLine();
-			userArray = user.toLowerCase().toCharArray();
+			
+			c.print("Your guess: ");
+			user = c.readLine();
+			user = user.toUpperCase();
+			userArray = user.toCharArray();
 			
 			correct = correctPlace(userArray, word, containsLetter(userArray, word));
 			
-			output = output(userArray, correctPlace(userArray, word, containsLetter(userArray, word)));
-
+			//Delete this method if not needed
+			//output = output(userArray, correctPlace(userArray, word, containsLetter(userArray, word)));
+			
+			//Decide box colour for letter
+			
+			int x = 230;
+			
+			for(int j = 0; j < 5; j ++) {
+				
+				if(correct[j] == 2) {
+					c.setColor(new Color(120, 178, 123));
+				}
+				else if(correct[j] == 1) {
+					c.setColor(new Color(219, 204, 90)); 
+				}
+				else {
+					c.setColor(Color.GRAY);
+				}
+				c.fillRect(x, 240 + 90 * i, 70, 70);
+				
+				c.setFont(new Font("SansSerif", Font.BOLD, 50));
+				c.setColor(Color.WHITE);
+				if(userArray[j] == 73) {
+					c.drawString(userArray[j] + "", x + 27, 295 + 90* i);
+				}
+				else {
+					c.drawString(userArray[j] + "", x + 17, 295 + 90* i);
+				}
+				
+				x += 90;
+			}
+			
+			//hi
+			
 			//Print out guess to grid
 			System.out.printf("%-5s%-5s%-5s%-5s%-5s\n\n", output[0], output[1], output[2], output[3], output[4]);
 			
 			if(word.equals(user)) {
+				//change to while so that we don't use break
+				//change when I use i for the y of the boxes and letters
 				break;
 			}
 			
@@ -139,15 +296,31 @@ public class WordlePrototype {
 		}
 		
 		if(guess < 6) {
-			System.out.println("You guessed the word!");
+			//c.println("You guessed the word!");
+			Thread.sleep(2000);
+			
+			c.setColor(new Color(120, 178, 123)); 
+			c.fillRect(0, 0, 1000, 1000);
+ 
+			c.setColor(Color.WHITE); 
+			c.setFont(new Font("SansSerif", Font.PLAIN, 70)); 
+			c.drawString("You guessed", 240, 425);
+			c.drawString("the word!", 300, 500);
+			
 		}
 		else {
-			System.out.println("The word was " + word + ". You lose :(");
+			
+			c.setColor(new Color(120, 178, 123)); 
+			c.fillRect(0, 0, 1000, 1000);
+			
+			c.setColor(Color.WHITE); 
+			c.setFont(new Font("SansSerif", Font.PLAIN, 70)); 
+			c.drawString("You ran out", 240, 425);
+			c.drawString("of guesses", 300, 500);
+			c.drawString("The word was " + word, 100, 700);
+			
 		}
 	
-		
-		
-
 
 		sc.close();
 
