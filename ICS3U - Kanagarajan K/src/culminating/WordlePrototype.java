1de56d8988ec0f27d69c3865412d8f61b50c5cda
Kaleena17 <Kaleena17@192.168.2.98>
2025-01-15 09:17:50 -0500

    This includes my WordlePrototype class.

--- ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
+++ ICS3U - Kanagarajan K/src/culminating/WordlePrototype.java
@@ -17,73 +17,60 @@ public class WordlePrototype {
 	 * @param args unused
 	 * @throws InterruptedException 
 	 */	
-	//static Console c; <- add this in real program so that the console applies to all methods?
 	public static void main(String[] args) throws InterruptedException {
-
-		/*
-		 * Things to add
-		 * - Handle User error
-		 * - Comment
-		 * - Graphics
-		 * - Music
-		 * - If I have time, add score storing method
-		 */
-		Console c = new Console(70, 150, 15, "Wordle");
-		Console s = new Console(10, 10, 15, "Input");
-
+		Console c = new Console(70, 150, 10, "Wordle");
 		Scanner sc = new Scanner(System.in);
+		
+		//Variables
 		String [] words = {"WEARY", "HAPPY", "MAGIC", "SHIFT", "RUINS"};
-		String [] output = new String [5];
-		int guess = 0;
-		String playAgain = "";
-
+		int guess;
 
 		//User input
 		String user = "";
 		char[] userArray = new char [5];
-
+		String playAgain = "";
 
 		//Chose a random word from words array 
 		int randomNum = (int) (Math.random()*(words.length));
 		String word = words[randomNum];
 		System.out.println("The word is: " + word);
 
+		// will holds the correctness of the user's guess
 		int [] correct = new int [5];
-
-
-
-
-
-		//Intro
-		/*
-		c.setColor(new Color(120, 178, 123)); 
-		c.fillRect(0, 0, 2000, 1000);
-
-		c.setColor(Color.WHITE); 
-		c.setFont(new Font("SansSerif", Font.PLAIN, 150)); 
-		c.drawString("Wordle", 420, 450);
-
-		Thread.sleep(1000); 
-		c.setFont(new Font("SansSerif", Font.PLAIN, 20));
-		c.drawString("Hit any key to continue", 550, 510);
-
-		c.getChar(); 
-		c.clear();
-		// 
-		
-		 
-		//Instructions 
 		
 		int x = 70;
 		int y = 100;
+		int x1 = 230;
+		int y1 = 240;
 
+
+		//Start Page
+		
+		c.setColor(new Color(120, 178, 123)); 
+		c.fillRect(0, 0, 1000, 1000);
+
+		c.setColor(Color.WHITE); 
+		c.setFont(new Font("SansSerif", Font.PLAIN, 125)); 
+		c.drawString("Wordle", 250, 450);
+
+		Thread.sleep(1000); 
+		c.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		c.drawString("Hit any key to continue", 350, 510);
+
+		c.getChar(); 
+		c.clear();
+		 
+		//Instructions Page 
+		// x and u variables are used here
+		
 		c.setColor(Color.BLACK);
 		c.setFont(new Font("SansSerif", Font.BOLD, 70));
 		c.drawString("How to Play", x, y);
 
 		c.setFont(new Font("SansSerif", Font.PLAIN, 30));
 		c.drawString("- You have 6 chances to guess the wordle", x + 30, y + 70);
-		c.drawString("- The colour of the tiles will change to show you how close you were", x + 30, y + 135);
+		c.drawString("- The colour of the tiles will change to show you how", x + 30, y + 115);
+		c.drawString("   close you were", x + 30, y + 155);
 
 		c.setFont(new Font("SansSerif", Font.BOLD, 40));
 		c.drawString("Examples", x, y + 220);
@@ -178,158 +165,248 @@ public class WordlePrototype {
 		//Continue
 		Thread.sleep(2000);
 		c.setFont(new Font("SansSerif", Font.BOLD, 35));
-		c.drawString("Hit any key to continue", x + 420, y + 790);
+		c.drawString("Hit any key to continue", x + 200, y + 790);
 		c.getChar();
-		*/
-
-
-		c.clear();
- 
-		//Player Grid
 		
-		int x1 = 400;
-		int y1 = 150;
-		
-		c.setColor(Color.BLACK);
 
-		c.setFont(new Font("SansSerif", Font.PLAIN, 70));
-		c.drawString("Wordle", x1 + 100, y1 - 50);
-
-		//Guess 1
-		c.drawRect(x1, y1, 70, 70);
-		c.drawRect(x1 + 90, y1, 70, 70);
-		c.drawRect(x1 + 180, y1, 70, 70);
-		c.drawRect(x1 + 270, y1, 70, 70);
-		c.drawRect(x1 + 360, y1, 70, 70);
-
-		//Guess 2
-		c.drawRect(x1, y1 + 90, 70, 70);
-		c.drawRect(x1 + 90, y1 + 90, 70, 70);
-		c.drawRect(x1 + 180, y1 + 90, 70, 70);
-		c.drawRect(x1 + 270, y1 + 90, 70, 70);
-		c.drawRect(x1 + 360, y1 + 90, 70, 70);
-
-		//Guess 3
-		c.drawRect(x1, y1 + 180, 70, 70);
-		c.drawRect(x1 + 90, y1 + 180, 70, 70);
-		c.drawRect(x1 + 180, y1 + 180, 70, 70);
-		c.drawRect(x1 + 270, y1 + 180, 70, 70);
-		c.drawRect(x1 + 360, y1 + 180, 70, 70);
-
-		//Guess 4
-		c.drawRect(x1, y1 + 270, 70, 70);
-		c.drawRect(x1 + 90, y1 + 270, 70, 70);
-		c.drawRect(x1 + 180, y1 + 270, 70, 70);
-		c.drawRect(x1 + 270, y1 + 270, 70, 70);
-		c.drawRect(x1 + 360, y1 + 270, 70, 70);
-
-		//Guess 5
-		c.drawRect(x1, y1 + 360, 70, 70);
-		c.drawRect(x1 + 90, y1 + 360, 70, 70);
-		c.drawRect(x1 + 180, y1 + 360, 70, 70);
-		c.drawRect(x1 + 270, y1 + 360, 70, 70);
-		c.drawRect(x1 + 360, y1 + 360, 70, 70);
-
-		//Guess 6
-		c.drawRect(x1, y1 + 450, 70, 70);
-		c.drawRect(x1 + 90, y1 + 450, 70, 70);
-		c.drawRect(x1 + 180, y1 + 450, 70, 70);
-		c.drawRect(x1 + 270, y1 + 450, 70, 70);
-		c.drawRect(x1 + 360, y1 + 450, 70, 70);
-
-
-		//for(int i = 0; i < 6; i++) {
-		while(guess < 6) {
-
-			//Get user input
+		do {
+			c.clear();
+	 
+			//Player Grid
+			//x1 and y1 variables are used here
 			
-			c.setCursor(30, 70);
-			c.print("Your guess: ");
-			c.setCursorVisible(true);
-			user = c.readLine();
-			user = user.toUpperCase();
+			c.setColor(Color.BLACK);
+	
+			c.setFont(new Font("SansSerif", Font.PLAIN, 70));
+			c.drawString("Wordle", x1 + 100, y1 - 50);
+	
+			//Guess 1
+			c.drawRect(x1, y1, 70, 70);
+			c.drawRect(x1 + 90, y1, 70, 70);
+			c.drawRect(x1 + 180, y1, 70, 70);
+			c.drawRect(x1 + 270, y1, 70, 70);
+			c.drawRect(x1 + 360, y1, 70, 70);
+	
+			//Guess 2
+			c.drawRect(x1, y1 + 90, 70, 70);
+			c.drawRect(x1 + 90, y1 + 90, 70, 70);
+			c.drawRect(x1 + 180, y1 + 90, 70, 70);
+			c.drawRect(x1 + 270, y1 + 90, 70, 70);
+			c.drawRect(x1 + 360, y1 + 90, 70, 70);
+	
+			//Guess 3
+			c.drawRect(x1, y1 + 180, 70, 70);
+			c.drawRect(x1 + 90, y1 + 180, 70, 70);
+			c.drawRect(x1 + 180, y1 + 180, 70, 70);
+			c.drawRect(x1 + 270, y1 + 180, 70, 70);
+			c.drawRect(x1 + 360, y1 + 180, 70, 70);
+	
+			//Guess 4
+			c.drawRect(x1, y1 + 270, 70, 70);
+			c.drawRect(x1 + 90, y1 + 270, 70, 70);
+			c.drawRect(x1 + 180, y1 + 270, 70, 70);
+			c.drawRect(x1 + 270, y1 + 270, 70, 70);
+			c.drawRect(x1 + 360, y1 + 270, 70, 70);
+	
+			//Guess 5
+			c.drawRect(x1, y1 + 360, 70, 70);
+			c.drawRect(x1 + 90, y1 + 360, 70, 70);
+			c.drawRect(x1 + 180, y1 + 360, 70, 70);
+			c.drawRect(x1 + 270, y1 + 360, 70, 70);
+			c.drawRect(x1 + 360, y1 + 360, 70, 70);
+	
+			//Guess 6
+			c.drawRect(x1, y1 + 450, 70, 70);
+			c.drawRect(x1 + 90, y1 + 450, 70, 70);
+			c.drawRect(x1 + 180, y1 + 450, 70, 70);
+			c.drawRect(x1 + 270, y1 + 450, 70, 70);
+			c.drawRect(x1 + 360, y1 + 450, 70, 70);
 			
-			while(user.length() != 5) {
-				c.setTextColor(Color.RED);
-				c.print("ERROR: Wrong input. Please try again\n");
-				c.setTextColor(Color.BLACK);
-				c.print("Your guess: ");
-				user = c.readLine();
+			//New console for user input
+			Console s = new Console(30, 20, 15, "Input");
+			
+			guess = 0;
+	
+			while(guess < 6) {
+				
+				//Get user input
+				
+				s.print("Your guess: ");
+				s.setCursorVisible(true);
+				user = s.readLine();
 				user = user.toUpperCase();
+				
+				//Check is user put correct input, and if not, handle it
+				while(user.length() != 5) {
+					
+					s.setTextColor(Color.RED);
+					s.print("ERROR: Wrong input. Please try again\n");
+					s.setTextColor(Color.BLACK);
+					s.print("Your guess: ");
+					user = s.readLine();
+					user = user.toUpperCase();
+				}
+				
+				//Turn user's input into an array of letter, so that it can be put in correctPlace and containsLetter Methods
+				userArray = user.toCharArray();
+	
+				correct = correctPlace(userArray, word, containsLetter(userArray, word));
+				
+				//Use x1 value to align boxes
+				//Re-intialize so that it is reset after every guess.
+				x1 = 230;
+	
+				for(int j = 0; j < 5; j ++) {
+					
+	
+					if(correct[j] == 2) {
+						c.setColor(new Color(120, 178, 123));
+					}
+					else if(correct[j] == 1) {
+						c.setColor(new Color(219, 204, 90)); 
+					}
+					else {
+						c.setColor(Color.GRAY);
+					}
+					c.fillRect(x1, y1 + 90 * guess, 70, 70);
+	
+					c.setFont(new Font("SansSerif", Font.BOLD, 50));
+					c.setColor(Color.WHITE);
+					if(userArray[j] == 73) {
+						c.drawString(userArray[j] + "", x1 + 27, 295 + 90* guess);
+					}
+					else {
+						c.drawString(userArray[j] + "", x1 + 17, 295 + 90* guess);
+					}
+	
+					x1 += 90;
+				}
+	
+				if(word.equals(user)) {
+					
+					//Pause before moving to next screen
+					Thread.sleep(1500);
+					
+					s.close();
+					c.clear();
+					
+					c.setColor(new Color(120, 178, 123)); 
+					c.fillRect(0, 0, 1000, 1000);
+	
+					c.setColor(Color.WHITE); 
+					c.setFont(new Font("SansSerif", Font.PLAIN, 70)); 
+					c.drawString("You guessed", 240, 425);
+					c.drawString("the word!", 300, 500);
+					
+					Thread.sleep(1000);
+					c.setFont(new Font("SansSerif", Font.PLAIN, 35));
+					c.drawString("Hit any key to continue", 260, 600);
+					c.getChar();
+					c.clear();
+					
+				
+					c.setColor(new Color(120, 178, 123)); 
+					c.fillRect(0, 0, 1000, 1000);
+					
+					c.setFont(new Font("SansSerif", Font.PLAIN, 70)); 
+					c.setColor(Color.WHITE); 
+					c.drawString("Do you want play again?", 100, 425);
+					c.drawString("Input y for yes and n for no", 50, 500);
+					
+					Console p = new Console (10, 20, 15, "Play Again");
+					p.print("You answer: ");
+					playAgain = p.readLine();
+					
+					//Check is user put correct input, and if not, handle it
+					while(!(playAgain.equalsIgnoreCase("y")) && !(playAgain.equalsIgnoreCase("n"))) {
+						
+						p.setTextColor(Color.RED);
+						p.print("ERROR: Wrong input. Please try again\n");
+						
+						p.setTextColor(Color.BLACK);
+						p.print("Your answer: ");
+						playAgain = p.readLine();
+					}
+					
+					p.close();
+					
+					guess = 6;
+				}
+	
+				guess ++;
+	
 			}
-			
-			userArray = user.toCharArray();
-
-			correct = correctPlace(userArray, word, containsLetter(userArray, word));
-
-			//Delete this method if not needed
-			//output = output(userArray, correctPlace(userArray, word, containsLetter(userArray, word)));
-
-			//Decide box colour for letter
-
-			int x2 = 230;
-
-			for(int j = 0; j < 5; j ++) {
-
-				if(correct[j] == 2) {
-					c.setColor(new Color(120, 178, 123));
-				}
-				else if(correct[j] == 1) {
-					c.setColor(new Color(219, 204, 90)); 
-				}
-				else {
-					c.setColor(Color.GRAY);
-				}
-				c.fillRect(x2, 240 + 90 * guess, 70, 70);
-
-				c.setFont(new Font("SansSerif", Font.BOLD, 50));
-				c.setColor(Color.WHITE);
-				if(userArray[j] == 73) {
-					c.drawString(userArray[j] + "", x2 + 27, 295 + 90* guess);
-				}
-				else {
-					c.drawString(userArray[j] + "", x2 + 17, 295 + 90* guess);
-				}
-
-				x2 += 90;
-			}
-
-			if(word.equals(user)) {
-
-				//c.println("You guessed the word!");
+	
+			if(!(word.equals(user))) {
+				
+				//Pause before moving to next screen
 				Thread.sleep(1500);
-
+				
+				s.close();
+				c.clear();
+				
 				c.setColor(new Color(120, 178, 123)); 
 				c.fillRect(0, 0, 1000, 1000);
-
+	
 				c.setColor(Color.WHITE); 
 				c.setFont(new Font("SansSerif", Font.PLAIN, 70)); 
-				c.drawString("You guessed", 240, 425);
-				c.drawString("the word!", 300, 500);
-
-				guess = 6;
+				c.drawString("You ran out of", 240, 350);
+				c.drawString("guesses", 320, 420);
+				c.setFont(new Font("SansSerif", Font.PLAIN, 70));
+				c.drawString("The word was: " + word.toLowerCase(), 140, 550);
+				
+				Thread.sleep(1000); 
+				c.setFont(new Font("SansSerif", Font.PLAIN, 35));
+				c.drawString("Hit any key to continue", 260, 650);
+				c.getChar();
+				
+				c.clear();
+				c.setColor(new Color(120, 178, 123)); 
+				c.fillRect(0, 0, 1000, 1000);
+				
+				
+				c.setFont(new Font("SansSerif", Font.PLAIN, 70)); 
+				c.setColor(Color.WHITE); 
+				c.drawString("Do you want play again?", 100, 425);
+				c.drawString("Input y for yes and n for no", 50, 500);
+				
+				Console p = new Console (10, 20, 15, "Play Again");
+				p.print("You answer: ");
+				playAgain = p.readLine();
+				
+				//Check is user put correct input, and if not, handle it
+				while(!(playAgain.equalsIgnoreCase("y")) && !(playAgain.equalsIgnoreCase("n"))) {
+					
+					p.setTextColor(Color.RED);
+					p.print("ERROR: Wrong input. Please try again\n");
+					
+					p.setTextColor(Color.BLACK);
+					p.print("Your answer: ");
+					playAgain = p.readLine();
+				}
+				
+				p.close();
+	
 			}
-
-			guess ++;
-
-		}
-
-		if(!(word.equals(user))) {
-
-			c.setColor(new Color(120, 178, 123)); 
-			c.fillRect(0, 0, 1000, 1000);
-
-			c.setColor(Color.WHITE); 
-			c.setFont(new Font("SansSerif", Font.PLAIN, 70)); 
-			c.drawString("You ran out of", 240, 350);
-			c.drawString("guesses", 320, 420);
-			c.setFont(new Font("SansSerif", Font.PLAIN, 70));
-			c.drawString("The word was: " + word.toLowerCase(), 140, 550);
-
-		}
-
-
-
+			
+		} while (playAgain.equalsIgnoreCase("y"));
+		
+		
+		
+		c.clear();
+		
+		//Ending Message
+		c.setColor(new Color(120, 178, 123)); 
+		c.fillRect(0, 0, 1000, 1000);
+		
+		c.setColor(Color.WHITE); 
+		c.setFont(new Font("SansSerif", Font.PLAIN, 70)); 
+		c.drawString("Thank You for Playing!", 100, 450);
+		
+		Thread.sleep(2000);
+		
+		c.close();
+		
 
 		sc.close();
 
